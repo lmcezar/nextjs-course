cb305297388c069bb67d93358e62e29de8945739
Lucas Cezar <lmcezar@gmail.com>
2024-06-12 09:42:08 -0300

    import articles data from json file

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,8 +1,39 @@
 import { PrismaClient } from "@prisma/client";
+import articles from "../../src/data/articles.json";
+import { slugify } from "../../src/helpers/slugify";
 
 const prisma = new PrismaClient();
+
+const isDev = process.env.NODE_ENV === "development";
 async function main() {
   console.log(`db seed`);
+  seedArticles();
+}
+
+async function seedArticles() {
+  if (!isDev) {
+    throw new Error("NODE_ENV is not a development environment");
+  }
+  // delete all articles and reset sequence number
+  await prisma.article.deleteMany();
+  await prisma.$executeRawUnsafe(
+    "DELETE FROM SQLITE_SEQUENCE WHERE name = $1;",
+    "Article"
+  );
+
+  for (let article of articles) {
+    const record = await prisma.article.create({
+      data: {
+        title: article.title,
+        slug: slugify(article.title),
+        excerpt: article.excerpt,
+        content: article.content,
+        image: article.image,
+        publishedAt: new Date(article.publish_date),
+      },
+    });
+    console.log("🚀 ~ seedArticles ~ record:", record.id, record.title);
+  }
 }
 
 main()
